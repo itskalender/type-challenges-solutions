526aa8596cd4f5f6babdf6cc57778906f5ce4bab
Kalender Toptas <toptaskalender@gmail.com>
2024-05-31 08:58:47 +0300

    533-concat

---
+++ solutions/easy/533-concat.ts
@@ -0,0 +1,24 @@
+/* _____________ Your Code Here _____________ */
+
+type Concat<T extends readonly unknown[], U extends readonly unknown[]> = [
+  ...T,
+  ...U
+];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = [1] as const;
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >
+];
